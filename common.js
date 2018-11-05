d31a610cf02f667ff17b0b01c3d0e9ed5ef90a55
Zack Guard <zacharyguard@gmail.com>
2018-11-05 23:34:49 +0800

    Move getSettings into common.js, rename

--- common.js
+++ common.js
@@ -2,6 +2,27 @@ let settings = {};
 
 const BULLET = "·";
 
+const getSettings = function() {
+  return new Promise(function(resolve, reject) {
+    let settings = {};
+
+    fetch(chrome.runtime.getURL("defaults.json")).then(response => {
+      return response.json();
+    }).then(defaults => {
+      let keys = Object.keys(defaults);
+      for (let key of keys) {
+        settings[key] = defaults[key].default;
+      }
+      chrome.storage.sync.get(keys, results => {
+        for (let key in results) {
+          settings[key] = results[key];
+        }
+        resolve([settings, defaults]);
+      });
+    }).catch(err => reject(err));
+  });
+}
+
 let onNavigate = (function() {
   let handlers = [];
   let oldURL = window.location.href;
