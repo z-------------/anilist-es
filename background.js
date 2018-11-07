635bcedf685dfa42b8f074e4ced03b632f1c2bfd
Zack Guard <zacharyguard@gmail.com>
2018-11-07 18:36:37 +0800

    notifs: click to open corresponding AniList page

--- background.js
+++ background.js
@@ -205,10 +205,10 @@ query ($page: Int, $types: [NotificationType]) {
 let variables = {
   page: 0,
   types: [
-    "ACTIVITY_MESSAGE", "ACTIVITY_REPLY", "FOLLOWING",
-    "ACTIVITY_MENTION", "THREAD_COMMENT_MENTION", "THREAD_SUBSCRIBED",
-    "THREAD_COMMENT_REPLY", "AIRING", "ACTIVITY_LIKE",
-    "ACTIVITY_REPLY_LIKE", "THREAD_LIKE", "THREAD_COMMENT_LIKE"
+    "ACTIVITY_MESSAGE", "ACTIVITY_REPLY", "ACTIVITY_LIKE", "ACTIVITY_MENTION", "ACTIVITY_REPLY_LIKE",
+    "THREAD_COMMENT_MENTION", "THREAD_SUBSCRIBED", "THREAD_COMMENT_REPLY", "THREAD_COMMENT_LIKE",
+    "THREAD_LIKE",
+    "FOLLOWING", "AIRING"
   ]
 };
 
@@ -236,17 +236,14 @@ function updateNotifs(cb) {
         .then(res => {
           res.json().then(json => {
             let notifs = json.data.Page.notifications;
-
             chrome.storage.local.get(["notifcache"], r => {
               let oldIds = [];
               if (r.notifcache) {
                 oldIds = JSON.parse(r.notifcache).map(notif => notif.id);
               }
               let ids = notifs.map(notif => notif.id);
-              // let newIds = [];
               for (let id of ids) {
                 if (oldIds.indexOf(id) === -1) {
-                  // newIds.push(id);
                   let notif = notifs.filter(function(notif) {
                     return notif.id === id;
                   })[0];
@@ -299,4 +296,37 @@ chrome.runtime.onMessage.addListener((request, sender, respond) => {
     });
   }
   return true; // allow async respond()
+});
+
+chrome.notifications.onClicked.addListener(notifId => {
+  let idSplit = notifId.split("_");
+  if (idSplit[0] === "anilist" && idSplit[1]) {
+    chrome.storage.local.get(["notifcache"], r => {
+      if (r.notifcache) {
+        let notifs = JSON.parse(r.notifcache);
+        let matches = notifs.filter(notif => notif.id === Number(idSplit[1]));
+        if (matches[0]) {
+          let notif = matches[0];
+          let url;
+          let typeSplit = notif.type.split("_");
+          if (typeSplit[0] === "ACTIVITY") {
+            url = `https://anilist.co/activity/${notif.activityId}`;
+          } else if (notif.type === "AIRING") {
+            url = `https://anilist.co/${notif.media.type.toLowerCase()}/${notif.media.id}`;
+          } else if (notif.type === "FOLLOWING") {
+            url = `https://anilist.co/user/${notif.user.name}`;
+          } else if (notifSplit[0] === "THREAD" && notifSplit[1] === "COMMENT" || notifSplit[1] === "SUBSCRIBED") {
+            url = `https://anilist.co/forum/thread/${notif.thread.id}/comment/${notif.commentId}`;
+          } else if (notif.type === "THREAD_LIKE") {
+            url = `https://anilist.co/forum/thread/${notif.threadId}`;
+          }
+          if (url) {
+            chrome.tabs.create({
+              url: url
+            });
+          }
+        }
+      }
+    });
+  }
 });
